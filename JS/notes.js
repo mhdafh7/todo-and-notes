77d13f79b5b4ace12660664cdebaf2b30d9f2bc0
Mhdafh <70367338+mhdafh7@users.noreply.github.com>
2021-03-17 10:22:11 +0530

    added editing feature

--- JS/notes.js
+++ JS/notes.js
@@ -22,67 +22,59 @@ noteList.addEventListener("click", editDelete);
 function modalAppear() {
   noteModalBg.classList.add("modal-visible");
   mainContainer.style.filter = "blur(10px)";
+  if(!noteModal.classList.contains("edit-modal")){
+    noteInput.value = "";
+    noteTitleInput.value = "";
+  }
 }
 function modalDismiss() {
+  if(noteModal.classList.contains("edit-modal")){
+    noteModal.classList.remove("edit-modal");
+  }
   noteModalBg.classList.remove("modal-visible");
   mainContainer.style.removeProperty("filter");
 }
 
 function addNote(event) {
   event.preventDefault();
-
-  const noteDiv = document.createElement("div");
-  // note li
-  const newNote = document.createElement("li");
-
-  if (noteTitleInput.value === "") {
-    noteDiv.innerText = "Untitled";
-  } else {
-    noteDiv.innerText = noteTitleInput.value;
-  }
-  noteDiv.classList.add("note");
-  newNote.classList.add("note-item");
-  noteDiv.appendChild(newNote);
-
-  // Store
   if (!noteModal.classList.contains("edit-modal")) {
-    Store(storeNote(noteDiv.innerText, noteInput.value));
+
+    const noteDiv = document.createElement("div");
+    // note li
+    const newNote = document.createElement("li");
+
+    noteDiv.appendChild(newNote);
+    if (noteTitleInput.value === "") {
+      newNote.innerText = "Untitled";
+    } else {
+      newNote.innerText = noteTitleInput.value;
+    }
+    noteDiv.classList.add("note");
+    newNote.classList.add("note-item");
+
+    // Store
+    Store(storeNote(newNote.innerText, noteInput.value));
     noteTitleInput.innerText = "";
     noteInput.innerText = "";
     modalDismiss();
-  } else {
-    let notes;
 
-    if (localStorage.getItem("notes") === null) {
-      notes = [];
-    } else {
-      notes = JSON.parse(localStorage.getItem("notes"));
-    }
-    const noteTitle = noteDiv.innerText;
-    let noteItem = notes.find((note) => note.title === noteTitle);
+    // Edit Button
+    const editButton = document.createElement("button");
+    editButton.classList.add("edit-btn");
+    editButton.innerHTML = '<i class="material-icons md-36">edit</i>';
+    noteDiv.appendChild(editButton);
 
-    noteItem.title = noteTitleInput.value;
-    noteItem.content = noteInput.value;
-    localStorage.setItem("notes", JSON.stringify(notes));
-    modalDismiss();
-    noteModal.classList.remove("edit-modal");
+    // Delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-btn");
+    deleteButton.innerHTML =
+      '<i class="material-icons md-36">delete_outline</i>';
+    noteDiv.appendChild(deleteButton);
+    noteList.appendChild(noteDiv);
+
+    noteInput.value = "";
+    noteTitleInput.value = "";
   }
-
-  // Edit Button
-  const editButton = document.createElement("button");
-  editButton.classList.add("edit-btn");
-  editButton.innerHTML = '<i class="material-icons md-36">edit</i>';
-  noteDiv.appendChild(editButton);
-
-  // Delete button
-  const deleteButton = document.createElement("button");
-  deleteButton.classList.add("delete-btn");
-  deleteButton.innerHTML = '<i class="material-icons md-36">delete_outline</i>';
-  noteDiv.appendChild(deleteButton);
-  noteList.appendChild(noteDiv);
-
-  noteInput.value = "";
-  noteTitleInput.value = "";
 }
 
 // edit and delete
@@ -99,6 +91,7 @@ function editDelete(e) {
   }
   // edit note
   if (item.classList[0] === "edit-btn") {
+    noteModal.classList.add("edit-modal");
     const note = item.parentElement;
     editNote(note);
   }
@@ -174,7 +167,6 @@ function removeNote(note) {
 
 // edit note
 function editNote(note) {
-  noteModal.classList.add("edit-modal");
   let notes;
 
   if (localStorage.getItem("notes") === null) {
@@ -183,9 +175,22 @@ function editNote(note) {
     notes = JSON.parse(localStorage.getItem("notes"));
   }
   const noteTitle = note.children[0].innerText;
+  console.log(noteTitle);
   let noteItem = notes.find((note) => note.title === noteTitle);
 
   modalAppear();
   noteTitleInput.value = noteItem.title;
   noteInput.value = noteItem.content;
+
+  let executed = false;
+  noteSaveButton.addEventListener("click", () => {
+    if (!executed) {
+      executed = true;
+      noteItem.title = noteTitleInput.value;
+      noteItem.content = noteInput.value;
+      localStorage.setItem("notes", JSON.stringify(notes));
+      note.children[0].innerText = noteItem.title;
+      modalDismiss();
+    }
+  });
 }
